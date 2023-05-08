be3840f417472b1d6affa25dac3986ed40789e1d
HRISHI313 <abishekbanerjee10@gmail.com>
2023-05-08 23:07:25 +0530

    data tranformation

--- src/pipeline/training_pipeline.py
+++ src/pipeline/training_pipeline.py
@@ -7,9 +7,15 @@ sys.path.insert(0, os.path.abspath(PROJECT_ROOT))
 from src.logger import logging
 from src.exception import CustomException
 from src.components.data_ingestion import Dataingestion
+from src.components.data_transformation import DataTransformation
 
 import pandas as pd
 
 if __name__=='__main__':
     obj = Dataingestion()
     train_data_path,test_data_path = obj.Initaite_Data_ingestion()
+
+    data_transformation = DataTransformation()
+    train_arr,test_arr,_= data_transformation.Initiate_data_transformation(train_data_path,test_data_path)
+
+
